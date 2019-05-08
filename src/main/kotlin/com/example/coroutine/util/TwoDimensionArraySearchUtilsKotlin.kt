34637c75d6d4f88c90b1c8e50ef0311185f160a2
Andrew Arnott <andrew@faire.com>
2019-05-08 10:05:10 -0400

    Improve Kotlin style

--- src/main/kotlin/com/example/coroutine/util/TwoDimensionArraySearchUtilsKotlin.kt
+++ src/main/kotlin/com/example/coroutine/util/TwoDimensionArraySearchUtilsKotlin.kt
@@ -1,30 +1,25 @@
 package com.example.coroutine.util
 
-class TwoDimensionArraySearchUtilsKotlin {
-
-    companion object {
-        fun findLargestNumberInArrayRange(startOfRange: Int, scanRangeSize: Int, array: Array<IntArray>): Int {
-            var largestNumber = Int.MIN_VALUE
-            for (x in startOfRange until (startOfRange + scanRangeSize)) {
-                for (y in array[x].indices) {
-                    if (largestNumber < array[x][y]) {
-                        largestNumber = array[x][y]
-                    }
-                }
+internal fun findLargestNumberInArrayRange(startOfRange: Int, scanRangeSize: Int, array: Array<IntArray>): Int {
+    var largestNumber = Int.MIN_VALUE
+    for (x in startOfRange until (startOfRange + scanRangeSize)) {
+        for (y in array[x].indices) {
+            if (largestNumber < array[x][y]) {
+                largestNumber = array[x][y]
             }
-
-            return largestNumber
-        }
-
-        fun findLargestNumberInResultArray(largestNumbersInRange: IntArray): Int {
-            var largestNumber = Int.MIN_VALUE
-            for (i in largestNumbersInRange.indices) {
-                if (largestNumber < largestNumbersInRange[i]) {
-                    largestNumber = largestNumbersInRange[i]
-                }
-            }
-
-            return largestNumber
         }
     }
+
+    return largestNumber
+}
+
+internal fun findLargestNumberInResultArray(largestNumbersInRange: IntArray): Int {
+    var largestNumber = Int.MIN_VALUE
+    for (i in largestNumbersInRange.indices) {
+        if (largestNumber < largestNumbersInRange[i]) {
+            largestNumber = largestNumbersInRange[i]
+        }
+    }
+
+    return largestNumber
 }
